d7b7f6f4aaccca76c6f737215501588aeea29a7d
joseph <josephjo1951210@gmail.com>
2023-11-20 15:44:59 +0100

    add admition to other function

--- AS3_3/src/main/java/com/security/PrintClient.java
+++ AS3_3/src/main/java/com/security/PrintClient.java
@@ -6,11 +6,16 @@ public class PrintClient {
     public static void main(String[] args) {
         try {
             PrintServer server = (PrintServer) Naming.lookup("rmi://localhost:1099/PrintServer");
-            String token = server.login("henry", "henry");
+            String token = server.login("george", "george");
             if (token != null) {
                 // Use the token returned from the server for subsequent operations
                 server.print("document.txt", "printer1", token);
                 System.out.println(server.queue("printer1", token));
+
+                System.out.println(server.queue("printer1", token));
+                String status = server.status("printer1", token);
+                System.out.println("Printer status: " + status);
+
             } else {
                 System.out.println("Login failed");
             }
